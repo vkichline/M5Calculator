1447143d30d8893621acf308ff76f714118978a9
Van Kichline <vkichline@hotmail.com>
2020-08-14 14:37:05 -0700

    implemented backspace

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,46 @@
 #include <M5Stack.h>
 
-#define KEYBOARD_I2C_ADDR     0X08        // I2C address of the Calculator FACE
-#define KEYBOARD_INT          5           // Data ready pin for Calculator FACE (active low)
-#define FG_COLOR              LIGHTGREY   // Arbitrary foreground color
-#define BG_COLOR              BLUE        // Arbitrary background color
-#define SCREEN_WIDTH          320         // Horizontal screen size
-#define SCREEN_H_CENTER       160         // Horizontal center of screen
+#define KEYBOARD_I2C_ADDR     0X08          // I2C address of the Calculator FACE
+#define KEYBOARD_INT          5             // Data ready pin for Calculator FACE (active low)
 
-#define ANN_TOP               4           // Top of the Annunciator, which shows calculator status
-#define ANN_HEIGHT            20          // Height of the Annunciator
-#define ANN_MARGIN            16          // Left/right margin for the Annunciator
-#define ANN_FONT              2           // Annunciator font
-#define ANN_FG_COLOR          BLACK       // Annunciator foreground color
-#define ANN_BG_COLOR          DARKGREY    // Annunciator background color
+#define SCREEN_WIDTH          320           // Horizontal screen size
+#define SCREEN_H_CENTER       160           // Horizontal center of screen
+#define FG_COLOR              LIGHTGREY     // Arbitrary foreground color
+#define BG_COLOR              BLUE          // Arbitrary background color
 
-#define ACC_TOP               40          // Top of the Accumulator display, where the sum is shown
-#define ACC_HEIGHT            40          // Height of the Accumulator
-#define ACC_MARGIN            16          // Left/right marging of the Accumulator
-#define ACC_FONT              6           // Accumulator font
-#define ACC_FG_COLOR          FG_COLOR    // Accumulator foreground color
-#define ACC_BG_COLOR          BG_COLOR    // Accumulator background color
+#define ANN_TOP               0             // Top of the Annunciator, which shows calculator status
+#define ANN_HEIGHT            20            // Height of the Annunciator
+#define ANN_V_MARGIN          2             // Offset from top to top text
+#define ANN_H_MARGIN          16            // Left/right margin for the Annunciator
+#define ANN_FONT              2             // Annunciator font
+#define ANN_FG_COLOR          BLACK         // Annunciator foreground color
+#define ANN_BG_COLOR          DARKGREY      // Annunciator background color
 
-#define INFO_TOP              110         // Top of the Info area
-#define INFO_HEIGHT           80          // Height of the Info area
-#define INFO_MARGIN           16          // Left/right margins of the Info area
-#define INFO_FONT             2           // Info font
-#define INFO_FG_COLOR         FG_COLOR    // Info foreground color
-#define INFO_BG_COLOR         BG_COLOR    // Info background color
+#define ACC_TOP               36            // Top of the Accumulator display, where the sum is shown
+#define ACC_HEIGHT            54            // Height of the Accumulator
+#define ACC_V_MARGIN          4             // Offset from top to top text
+#define ACC_H_MARGIN          16            // Left/right marging of the Accumulator
+#define ACC_FONT              6             // Accumulator font
+#define ACC_FG_COLOR          FG_COLOR      // Accumulator foreground color
+#define ACC_BG_COLOR          BG_COLOR      // Accumulator background color
 
+#define INFO_TOP              108           // Top of the Info area
+#define INFO_HEIGHT           80            // Height of the Info area
+#define INFO_H_MARGIN         16            // Left/right margins of the Info area
+#define INFO_V_MARGIN         2             // Offset from top to top text
+#define INFO_FONT             2             // Info font
+#define INFO_FG_COLOR         FG_COLOR      // Info foreground color
+#define INFO_BG_COLOR         BG_COLOR      // Info background color
+
+#define LABEL_TOP             220           // Top of the button labels
+#define LABEL_HEIGHT          22            // Height of the button labels
+#define LABEL_V_MARGIN        2             // Offset from top to top text
+#define LABEL_FONT            2             // Font for the button labels
+#define LABEL_FG_COLOR        ANN_FG_COLOR  // Button labels foreground color
+#define LABEL_BG_COLOR        ANN_BG_COLOR  // Button labels background color
+#define LABEL_BTN_A_CENTER    68            // Vertical center for Button A
+#define LABEL_BTN_B_CENTER    160           // Vertical center for Button B
+#define LABEL_BTN_C_CENTER    252           // Vertical center for Button C
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -47,6 +60,7 @@ enum Calc_Command {
   PERCENT,      // Depends on status of command; see function comments
   SIGN          // Reverse the sign of the accumulator
 };
+
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -135,6 +149,15 @@ bool is_deferred_command(Calc_Command cmd) {
 
 ////////////////////////////////////////////////////////////////////////////////
 //
+//  Return true if the user can backspace in the accumulator
+//
+bool can_backspace() {
+  return !restart && accumulator != "0" && accumulator != "";
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
 //  Display Routines
 //
 //  The display has four distinct parts:
@@ -144,19 +167,6 @@ bool is_deferred_command(Calc_Command cmd) {
 //  Button Labels title the A/B/C buttons
 //
 ////////////////////////////////////////////////////////////////////////////////
-
-
-////////////////////////////////////////////////////////////////////////////////
-//
-//  Show the main number of interest near the top of the screen.
-//
-void display_accumulator() {
-  M5.Lcd.fillRect(0, ACC_TOP, SCREEN_WIDTH, ACC_HEIGHT, ACC_BG_COLOR);
-  M5.Lcd.setTextColor(ACC_FG_COLOR, ACC_BG_COLOR);  // Blank space erases background w/ background color set
-  M5.Lcd.setTextDatum(TR_DATUM);                    // Print right-justified, relative to end of string
-  M5.Lcd.drawString(accumulator, SCREEN_WIDTH - ACC_MARGIN, ACC_TOP, ACC_FONT);
-  M5.Lcd.setTextDatum(TL_DATUM);                    // Go back to normal text alignment
-}
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -175,10 +185,9 @@ void clear_info() {
 void display_memory_info() {
   if(!memory_mode) return;
   M5.Lcd.setTextColor(INFO_FG_COLOR, INFO_BG_COLOR);  // Blank space erases background w/ background color set
-  M5.Lcd.setCursor(INFO_MARGIN, INFO_TOP, INFO_FONT);
-  M5.Lcd.drawCentreString("Memory Commands", SCREEN_H_CENTER, INFO_TOP, INFO_FONT);
-  M5.Lcd.drawCentreString("M M  Recall      M =  Save      M AC  Clear", SCREEN_H_CENTER, INFO_TOP + 30, INFO_FONT);
-  M5.Lcd.drawCentreString("Also  M+  M-  M*  M/ to change Memory", SCREEN_H_CENTER, INFO_TOP + 55, INFO_FONT);
+  M5.Lcd.drawCentreString("Memory Commands", SCREEN_H_CENTER, INFO_TOP + INFO_V_MARGIN, INFO_FONT);
+  M5.Lcd.drawCentreString("M M  Recall      M =  Save      M AC  Clear", SCREEN_H_CENTER, INFO_TOP + INFO_V_MARGIN + 30, INFO_FONT);
+  M5.Lcd.drawCentreString("Also  M+  M-  M*  M/ to change Memory", SCREEN_H_CENTER, INFO_TOP + INFO_V_MARGIN + 55, INFO_FONT);
 }
 
 
@@ -192,7 +201,7 @@ void display_annunciator() {
   String display = "                ";                        // With background text color set, this does erasing for us.
   bool memory_is_clear   = memory   == "0" || memory   == ""; // True if nothing stored in memory.
   bool opperand_is_clear = opperand == "0" || opperand == ""; // True if nothing stored in the operand.
-  M5.Lcd.fillRect(0, 0, SCREEN_WIDTH, ANN_TOP + ANN_HEIGHT, ANN_BG_COLOR);  // Erase from the top of the screen
+  M5.Lcd.fillRect(0, 0, SCREEN_WIDTH, ANN_HEIGHT, ANN_BG_COLOR);  // Erase from the top of the screen
   if(memory_mode) display += "M";                             // Show M if entering a memory command.
   if(!opperand_is_clear) display += " " + opperand;           // Display the number we're opperating one
   switch(command) {
@@ -204,9 +213,9 @@ void display_annunciator() {
   }
   M5.Lcd.setTextColor(ANN_FG_COLOR, ANN_BG_COLOR);            // Blank space erases background w/ background color set
   M5.Lcd.setTextDatum(TR_DATUM);                              // Print right-justified, relative to end of string
-  M5.Lcd.drawString(display, SCREEN_WIDTH - ANN_MARGIN, ANN_TOP, ANN_FONT);
+  M5.Lcd.drawString(display, SCREEN_WIDTH - ANN_H_MARGIN, ANN_TOP + ANN_V_MARGIN, ANN_FONT);
   M5.Lcd.setTextDatum(TL_DATUM);                              // Go back to normal text alignment
-  M5.Lcd.setCursor(ANN_MARGIN, ANN_TOP, ANN_FONT);
+  M5.Lcd.setCursor(ANN_H_MARGIN, ANN_TOP + ANN_V_MARGIN, ANN_FONT);
   if(!memory_is_clear) {
     M5.Lcd.print(memory);                                     // Show memory in upper left corner
   }
@@ -214,6 +223,32 @@ void display_annunciator() {
   if(memory_mode) display_memory_info();                      // Display help on using memory
 }
 
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Show the main number of interest near the top of the screen.
+//
+void display_accumulator() {
+  M5.Lcd.fillRect(0, ACC_TOP, SCREEN_WIDTH, ACC_HEIGHT, ACC_BG_COLOR);
+  M5.Lcd.setTextColor(ACC_FG_COLOR, ACC_BG_COLOR);  // Blank space erases background w/ background color set
+  M5.Lcd.setTextDatum(TR_DATUM);                    // Print right-justified, relative to end of string
+  M5.Lcd.drawString(accumulator, SCREEN_WIDTH - ACC_H_MARGIN, ACC_TOP + ACC_V_MARGIN, ACC_FONT);
+  M5.Lcd.setTextDatum(TL_DATUM);                    // Go back to normal text alignment
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Show the button labels, which may be state dependent
+//
+void display_button_labels() {
+  M5.Lcd.setTextColor(LABEL_FG_COLOR, LABEL_BG_COLOR);  // Blank space erases background w/ background color set
+  M5.Lcd.fillRect(0, LABEL_TOP, SCREEN_WIDTH, LABEL_HEIGHT, LABEL_BG_COLOR);
+  if(can_backspace()) M5.Lcd.drawCentreString("BKSPC", LABEL_BTN_A_CENTER, LABEL_TOP + LABEL_V_MARGIN, LABEL_FONT);
+  // M5.Lcd.drawCentreString("V", LABEL_BTN_B_CENTER, LABEL_TOP + LABEL_V_MARGIN, LABEL_FONT);
+  // M5.Lcd.drawCentreString("V", LABEL_BTN_C_CENTER, LABEL_TOP + LABEL_V_MARGIN, LABEL_FONT);
+}
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -266,6 +301,7 @@ void perform_operation() {
         accumulator = String("-") + accumulator;
       break;
     default:
+      restart     = true;   // New numbers replace accumulator rather than add to it.
       break;
   }
   command = NO_COMMAND;
@@ -431,6 +467,20 @@ void process_calculator_command(Calc_Command cmd) {
 
 ////////////////////////////////////////////////////////////////////////////////
 //
+//  Handle a button press
+//  Currently there is only one button action; backspace on the A button
+//
+void process_button(uint8_t button) {
+  if(BUTTON_A == button && can_backspace()) {
+    accumulator.remove(accumulator.length() - 1);               // Remove the last character
+    accumulator = double_to_string(atof(accumulator.c_str()));  // Fixes up all kinds of problems
+    display_accumulator();
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
 //  Handle a command from user input. Decide which processor gets  the command.
 //  If it's non-memory-mode, determine if we need to execute a pending command
 //  first (for example: 1 + 1 + 1.)
@@ -444,7 +494,7 @@ void process_command(Calc_Command cmd) {
   else {
     // If the user presses +, then *, allow the processor to change + to *.
     // Otherwise, we need to see if there's a pending command to execute.
-    if(!is_deferred_command(previous)) {
+    if(is_deferred_command(cmd) && !is_deferred_command(previous)) {
       if(is_deferred_command(command)) {
         perform_operation();
       }
@@ -493,12 +543,17 @@ bool read_key(char& input) {
 
 ////////////////////////////////////////////////////////////////////////////////
 //
-//  Read a key from the keyboard.
+//  Read a key from the keyboard, or a button from the M5Stack.
 //  If it's a digit, push it into the accumulator and display it.
 //  If it's a command, execute it.
 //
 bool process_input() {
   char input;
+  M5.update();
+  if(M5.BtnA.wasReleased()) { process_button(BUTTON_A); return true; }
+  if(M5.BtnB.wasReleased()) { process_button(BUTTON_B); return true; }
+  if(M5.BtnC.wasReleased()) { process_button(BUTTON_C); return true; }
+
   if(read_key(input)) {
     if(is_digit(input)) process_digit((uint8_t)input - '0');
     else {
@@ -534,6 +589,7 @@ void setup() {
   M5.Lcd.fillScreen(BG_COLOR);
   display_accumulator();
   display_annunciator();
+  display_button_labels();
 }
 
 
@@ -544,6 +600,8 @@ void setup() {
 //
 void loop() {
   // If anything happend, make sure the annunciator is updated
-  if(process_input()) display_annunciator();
-  delay(10);
+  if(process_input()) {
+    display_annunciator();
+    display_button_labels();
+  }
 }
